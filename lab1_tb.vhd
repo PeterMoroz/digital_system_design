16dd2c78dab7ddb22c0306c99c4d3d3e1c54d93b
Peter Moroz <ptr.moroz@gmail.com>
2024-01-18 14:28:07 +0300

    lab1 - add half-adder, full-adder

--- lab1_tb.vhd
+++ lab1_tb.vhd
@@ -35,15 +35,42 @@ ARCHITECTURE behavior OF lab1_tb IS
         b : in std_logic;
         q : out std_logic
      );
-    END COMPONENT;    
+    END COMPONENT;
+
+
+    COMPONENT half_adder
+
+    PORT( a : in std_logic;
+        b : in std_logic;
+        s : out std_logic;
+        c : out std_logic
+    );
+    END COMPONENT;
+
+
+    COMPONENT full_adder
+
+    PORT( a : in std_logic;
+        b : in std_logic;
+        cin : in std_logic;
+        s : out std_logic;
+        cout : out std_logic
+    );
+    END COMPONENT;
 
    --Signal definitions: Declare (and initialize) a signal for each port of the UUT. 
 
     signal a: std_logic := 'U';
     signal b: std_logic := 'U';
+    signal c: std_logic := 'U';
     signal q_and: std_logic := 'U';
     signal q_or: std_logic := 'U';
     signal q_xor: std_logic := 'U';
+
+    signal s1: std_logic := 'U';
+    signal c1: std_logic := 'U';
+    signal s2: std_logic := 'U';
+    signal c2: std_logic := 'U';
 
 BEGIN
 
@@ -60,12 +87,27 @@ BEGIN
     b => b,
     q => q_or
     );
-    
+
    uut3: xor_gate PORT MAP (
      a => a,
      b => b,
      q => q_xor
-     );    
+     );
+
+    uut4: half_adder PORT MAP (
+     a => a,
+     b => b,
+     s => s1,
+     c => c1
+    );
+
+    uut5: full_adder PORT MAP (
+     a => a,
+     b => b,
+     s => s2,
+     cin => c,
+     cout => c2
+    );
     
    -- Stimulus process
   stim_proc: process
@@ -79,6 +121,29 @@ BEGIN
         b <= '1';
         wait for 10 ns;
         a <= '1';
+        wait for 20 ns;
+
+        a <= '0';
+        b <= '0';
+        c <= '0';
+        wait for 10 ns;
+        a <= '1';
+        wait for 10 ns;
+        a <= '0';
+        b <= '1';
+        wait for 10 ns;
+        a <= '0';
+        b <= '0';
+        c <= '1';
+        wait for 10 ns;
+        a <= '1';
+        wait for 10 ns;
+        a <= '0';
+        b <= '1';
+        wait for 10 ns;
+        a <= '1';
+        wait for 10 ns;
+        c <= '0';
         wait;
   end process;
 
